eecf31b3f8a4691dafcd0b03afc99cc741eb53f1
i.gorin <i.gorin@g.nsu.ru>
2022-10-24 23:01:17 +0700

    init commit

---
+++ lab5/lab5/src/main/java/ru/nsu/gorin/networks/lab5/Main.java
@@ -0,0 +1,13 @@
+package ru.nsu.gorin.networks.lab5;
+
+import ru.nsu.gorin.networks.lab5.proxy.ProxySocks;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args) throws UnknownHostException {
+        ProxySocks proxy = new ProxySocks(InetAddress.getLocalHost(), 8185);
+        proxy.run();
+    }
+}
